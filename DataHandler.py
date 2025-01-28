d7839d36b55259629bde116f0644e88babf68755
lyyccccccc <130274073+lyyccccccc@users.noreply.github.com>
2025-01-28 18:50:49 +0800

    Update DataHandler.py

--- DataHandler.py
+++ DataHandler.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.sparse import csr_matrix
 from Params import args
 import scipy.sparse as sp
-from Utils.TimeLogger import log
+from Utils.Logger import log
 import os
 import random
 def transpose(mat):
